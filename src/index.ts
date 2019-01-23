bf74020f65cf990d31e9ff621f23fd635e9a1d4d
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-23 18:20:12 +0000

    update package.json

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ const server = http.createServer(function (req, res) {
       res.writeHead(200, { 'Content-Type': 'application/json' })
       res.end(
         JSON.stringify({
-          name: 'WalletConnect Bridge Server',
-          repository: 'walletconnect-bridge',
+          name: pkg.name,
+          description: pkg.description,
           version: pkg.version
         })
       )
